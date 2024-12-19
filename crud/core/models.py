6ba214c5038547d0c085ce578281c8242222dcb7
vaishnavidubbalwar2001 <vaishnavidubbalwar18@gmail.com>
2024-12-19 16:15:44 +0530

    feat sql lite database

--- crud/core/models.py
+++ crud/core/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=50)
     roll = models.IntegerField()
-    Maths = models.IntegerField()
-    Science = models.IntegerField()
-    English = models.IntegerField()
+    # Maths = models.IntegerField()
+    # Science = models.IntegerField()
+    # English = models.IntegerField()
     city = models.CharField(max_length=50)
 
--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -74,29 +74,29 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'NAME':  'vaishnavi_db',
+#         'USER':  'metafit',
+#         'PASSWORD': 'paHgayi867',
+#         'HOST': '13.233.76.220',
+#         'PORT':  '3306',
+#         'OPTIONS': {
+#             # 'sql_mode': 'traditional',
+#             'sql_mode': 'STRICT_TRANS_TABLES'
+#         },
 #     }
 # }
-
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':  'vaishnavi_db',
-        'USER':  'metafit',
-        'PASSWORD': 'paHgayi867',
-        'HOST': '13.233.76.220',
-        'PORT':  '3306',
-        'OPTIONS': {
-            # 'sql_mode': 'traditional',
-            'sql_mode': 'STRICT_TRANS_TABLES'
-        },
-    }
-}
 
 
 
