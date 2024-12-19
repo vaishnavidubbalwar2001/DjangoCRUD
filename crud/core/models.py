1b0e44006a7a4b0cede80e1ba6d1971b0e8d71ad
vaishnavidubbalwar2001 <vaishnavidubbalwar18@gmail.com>
2024-12-19 15:04:58 +0530

    first commit

---
+++ crud/core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    roll = models.IntegerField()
+    Maths = models.IntegerField()
+    Science = models.IntegerField()
+    English = models.IntegerField()
+    city = models.CharField(max_length=50)
+
